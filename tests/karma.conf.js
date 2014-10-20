4d837ee2df9ce56609260ac218961f2df3805389
Devon Maloney <devon.maloney@vokalinteractive.com>
2014-10-20 15:19:09 -0500

    Unit tests, package.json, and .gitignore

---
+++ tests/karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function ( config )
+{
+    config.set( {
+
+        basePath: "../",
+        frameworks: [ "jasmine" ],
+        autoWatch: false,
+        browsers: [ "PhantomJS" ],
+        reporters: [ "dots" ],
+        singleRun: true,
+        plugins: [
+            "karma-jasmine",
+            "karma-phantomjs-launcher"
+        ],
+
+        files: [
+            "source/components/angular/angular.js",
+            "source/components/angular-mocks/angular-mocks.js",
+            "source/components/humps/humps.js",
+
+            "services_API.js",
+            "services_humps.js",
+
+            "tests/*.spec.js"
+        ]
+
+    } );
+    
+};
