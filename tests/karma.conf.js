5acd3e8a0c1548dfd517b477ea338f1bd9645fa3
Jarrett Widman <jarrett@24m2.com>
2014-11-26 16:02:47 -0600

    add coverage reporter

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -8,13 +8,23 @@ module.exports = function ( config )
         frameworks: [ "jasmine" ],
         autoWatch:  false,
         browsers:   [ "PhantomJS" ],
-        reporters:  [ "dots" ],
+        reporters:  [ "dots", "coverage" ],
         singleRun:  true,
         plugins: [
             "karma-jasmine",
-            "karma-phantomjs-launcher"
+            "karma-phantomjs-launcher",
+            "karma-coverage"
         ],
-
+        preprocessors: {
+            "services_API.js": [ "coverage" ],
+            "services_humps.js": [ "coverage" ]
+        },
+        coverageReporter: {
+            dir : "coverage/",
+            reporters: [
+                { type: 'cobertura', subdir: '.', file: 'cobertura.xml' },
+            ]
+        },
         files: [
             "source/components/angular/angular.js",
             "source/components/angular-mocks/angular-mocks.js",
