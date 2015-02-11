d929e2995e39396a9c108b04c71fc3c2fb94738f
Cory Faller <cory.faller@vokalinteractive.com>
2015-02-11 15:08:55 -0600

    Let's see how this works

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function( grunt )
+{
+    "use strict";
+
+    grunt.initConfig( {
+
+        pkg: grunt.file.readJSON( "package.json" ),
+
+        uglify:
+        {
+            options:
+            {
+                mangle: false,
+                compress: true,
+                banner: "/*! <%= pkg.name %> Copyright Vokal Interactive <%= grunt.template.today( 'yyyy' ) %> */",
+                sourceMap: false
+            },
+            all:
+            {
+                files:
+                {
+                    "dist/vokal-ng-api.min.js": [
+                        "source/*"
+                    ]
+                }
+            }
+        },
+
+        githooks:
+        {
+            all:
+            {
+                "pre-commit": "uglify"
+            }
+        }
+
+    } );
+
+    // Load plugins
+    grunt.loadNpmTasks( "grunt-contrib-uglify" );
+    grunt.loadNpmTasks( "grunt-githooks" );
+
+};
