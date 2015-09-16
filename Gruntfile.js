e794af27f60a117f20bb85634e9281f773679b89
Jarrett Widman <jarrett@24m2.com>
2015-09-16 12:58:09 -0500

    UMD wrapper

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,21 +6,46 @@ module.exports = function( grunt )
 
         pkg: grunt.file.readJSON( "package.json" ),
 
+        concat:
+        {
+            dist:
+            {
+                files:
+                {
+                    "dist/<%= pkg.name %>.js": [
+                        "source/*.*"
+                    ]
+                }
+            }
+        },
+
+        umd: {
+            dist: {
+                options: {
+                    src: "dist/<%= pkg.name %>.js",
+                    amdModuleId: "<%= pkg.name %>",
+                    deps: {
+                        "default": [ "humps" ]
+                    }
+                }
+            }
+        },
+
         uglify:
         {
             options:
             {
                 mangle: true,
-                compress: true,
+                compress: {},
                 banner: "/*! <%= pkg.name %> Copyright Vokal <%= grunt.template.today( 'yyyy' ) %> */\n",
                 sourceMap: false
             },
-            all:
+            dist:
             {
                 files:
                 {
-                    "dist/vokal-ng-api.min.js": [
-                        "source/*.*"
+                    "dist/<%= pkg.name %>.min.js": [
+                        "dist/<%= pkg.name %>.js"
                     ]
                 }
             }
@@ -30,5 +55,9 @@ module.exports = function( grunt )
 
     // Load plugins
     grunt.loadNpmTasks( "grunt-contrib-uglify" );
+    grunt.loadNpmTasks( "grunt-contrib-concat" );
+    grunt.loadNpmTasks( "grunt-umd" );
+
+    grunt.registerTask( "default", [ "concat", "umd", "uglify" ] );
 
 };
